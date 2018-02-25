73d2a9a8be2c5b263e2444089b82ee160596e36f
swensun <1416316137@qq.com>
2018-02-25 13:38:55 +0800

    update linear regression

--- machine_learning_algorithm/linear regression/index.py
+++ machine_learning_algorithm/linear regression/index.py
@@ -27,14 +27,14 @@ def init_data():
 
 
 def linear_regression():
-    learning_rate = 0.001
-    initial_b = 1.244
-    initial_m = 1.866
-    num_iter = 1000
+    learning_rate = 0.01 #步长
+    initial_b = 0
+    initial_m = 0
+    num_iter = 1000 #迭代次数
 
     data = init_data()
-    [b, m] = optimizer(data, initial_b, initial_m, learning_rate, num_iter)
-    # plot_data(data,b,m)
+    [b, m] = optimizer_two(data, initial_b, initial_m, learning_rate, num_iter)
+    plot_data(data,b,m)
     print(b, m)
     return b, m
 
@@ -44,12 +44,7 @@ def optimizer(data, initial_b, initial_m, learning_rate, num_iter):
     m = initial_m
 
     for i in range(num_iter):
-        # before = computer_error(b, m, data)
         b, m = compute_gradient(b, m, data, learning_rate)
-        # after = computer_error(b, m, data)
-        # if after > before:
-        #     print(i)
-        #     break
         if i % 100 == 0:
             print(i, computer_error(b, m, data)) # 损失函数，即误差
     return [b, m]
@@ -69,16 +64,39 @@ def compute_gradient(b_cur, m_cur, data, learning_rate):
         b_gradient += -(2 / N) * (y - ((m_cur * x) + b_cur))
         m_gradient += -(2 / N) * x * (y - ((m_cur * x) + b_cur)) #偏导数
 
-    # delta = 0.0000001
+    new_b = b_cur - (learning_rate * b_gradient)
+    new_m = m_cur - (learning_rate * m_gradient)
+    return [new_b, new_m]
 
-    # for i in range(len(data)):
-    #     x = data[i, 0]
-    #     y = data[i, 1]
-    #     b_gradient = (error(x, y, b_cur + delta, m_cur) - error(x, y, b_cur - delta, m_cur)) / (2*delta)
-    #     m_gradient = (error(x, y, b_cur, m_cur + delta) - error(x, y, b_cur, m_cur - delta)) / (2*delta)
-    #
-    # b_gradient = b_gradient / N
-    # m_gradient = m_gradient / N
+def optimizer_two(data, initial_b, initial_m, learning_rate, num_iter):
+    b = initial_b
+    m = initial_m
+
+    while True:
+        before = computer_error(b, m, data)
+        b, m = compute_gradient(b, m, data, learning_rate)
+        after = computer_error(b, m, data)
+        if abs(after - before) < 0.0000001:
+            break
+    return [b, m]
+
+def compute_gradient_two(b_cur, m_cur, data, learning_rate):
+    b_gradient = 0
+    m_gradient = 0
+
+    N = float(len(data))
+
+    delta = 0.0000001
+
+    for i in range(len(data)):
+        x = data[i, 0]
+        y = data[i, 1]
+        # 利用导数的定义来计算梯度
+        b_gradient = (error(x, y, b_cur + delta, m_cur) - error(x, y, b_cur - delta, m_cur)) / (2*delta)
+        m_gradient = (error(x, y, b_cur, m_cur + delta) - error(x, y, b_cur, m_cur - delta)) / (2*delta)
+
+    b_gradient = b_gradient / N
+    m_gradient = m_gradient / N
     #
     new_b = b_cur - (learning_rate * b_gradient)
     new_m = m_cur - (learning_rate * m_gradient)
@@ -119,17 +137,10 @@ def scikit_learn():
     linreg.fit(x, y)
     print(linreg.coef_)
     print(linreg.intercept_)
-    print(linreg.score(x, y))
-    print(linreg.n_jobs)
 
-    # b, m = linear_regression()
-    #
-    # y_predict = m * x + b
-    #
-    # print(linreg.score(x, y_predict))
 
 if __name__ == '__main__':
-    linear_regression()
+    # linear_regression()
     scikit_learn()
 
 
