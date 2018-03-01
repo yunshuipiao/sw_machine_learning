63ed96d23eec6ab77cab85344864a81b9e07fb7c
swensun <1416316137@qq.com>
2018-03-01 21:10:46 +0800

    skip svm

---
+++ machine_learning_algorithm/svm/index.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+"""
+@author: swensun
+
+@github:https://github.com/yunshuipiao
+
+@software: python
+
+@file: index.py
+
+@desc: svm
+
+@hint: skip
+"""
+
+
