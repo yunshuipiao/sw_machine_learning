17303f04e840e8ce4fe5aee91e350fb7ab7888d3
swensun <1416316137@qq.com>
2018-03-02 19:36:53 +0800

    skip random forest

---
+++ machine_learning_algorithm/random_forest/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+"""
+@author: swensun
+
+@github:https://github.com/yunshuipiao
+
+@software: python
+
+@file: __init__.py.py
+
+@desc:
+
+@hint:
+"""
---
+++ machine_learning_algorithm/random_forest/index.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+"""
+@author: swensun
+
+@github:https://github.com/yunshuipiao
+
+@software: python
+
+@file: index.py
+
+@desc: 随机森林
+
+@hint: skip
+"""
+
