27e95fef1be1a74dc0d63e8835a67401e2205721
swensun <1416316137@qq.com>
2018-03-13 23:27:14 +0800

    update ann

--- machine_learning_algorithm/ANN/index.py
+++ machine_learning_algorithm/ANN/index.py
@@ -27,13 +27,69 @@ def tanh_deriv(x):
     return 1.0 - np.tanh(x) * np.tanh(x)
 
 #逻辑函数
-def logistic(x):
+def logistics(x):
     return 1 / (1 + np.exp(-x))
 
 #逻辑函数的微分
 def logistic_derivatice(x):
-    return logistic(x) * (1 - logistic(x))
+    return logistics(x) * (1 - logistics(x))
 
 #构建ANN
 class NeuralNetwork:
-    pass
+    # 构造函数 layers表示有多少层，每层有多少个神经元。
+    # acvitation 为使用的激活函数名称，有默认值tanh.
+    def __init__(self, layers, activation = 'tanh'):
+        if activation == 'logisitic':
+            self.activation = logistics
+            self.activation_detiv = logistic_derivatice
+        else:
+            self.activation = tanh
+            self.activation_detiv = tanh_deriv
+        self.weight = []
+        #len(layers)-1的目的是 输出层不需要赋予权值
+        for i in range(1, len(layers) - 1):
+            # 对当前层与前一层之间的连线进行权重赋值， 在-0.25-0.25之间
+            self.weight.append( (2 * np.random.random((layers[i - 1 ] + 1, layers[i] + 1)) - 1) * 0.25 )
+            self.weight.append( (2 * np.random.random((layers[i] + 1, layers[i + 1] + 1)) - 1) * 0.25 )
+
+    def fit(self, X, y, learning_rate = 0.2, epochs = 10000):
+        # X表示训练集， 通常模拟成一个二维矩阵，每一层代表一个样本的不同特征
+        # 每一列代表不同的样本， y指的是classLabel， 表示输出的分类标记
+        # epochs表示循环次数
+
+        # 将X转为numpy2维数组，至少二维
+        X = np.atleast_2d(X)
+
+        temp = np.ones([X.shape[0], X.shape[1] + 1])
+
+        temp[:, 0:-1] = X
+        X = temp
+        y = np.array(y)
+
+        for k in range(epochs):
+            i = np.random.randint(X.shape[0])
+            a = [X[i]]  # a是从x中任意抽取的一行数据
+
+            #正向更新
+            for l in range(len(self.weight)): #循环遍历每一层
+                a.append(self.activation(np.dot(a[l], self.weight[1])))
+
+            error = y[i] - a[-1]
+            deltas = [error * self.activation_detiv(a[-1])]
+
+            #反向传播
+            for l in range(len(a) - 2, 0, -1):
+                deltas.append(deltas[-1].dot(self.weight[l].T) * self.activation_detiv(a[l]))
+            deltas.reverse()
+
+            for i in range(len(self.weight)):
+                layer = np.atleast_2d(a[i])
+                delta = np.atleast_2d(deltas[i]) #delta存的是误差
+                self.weight[i] += learning_rate * layer.T.dot(delta)
+
+
+if __name__ == '__main__':
+    # i = (2 * np.random.random((2, 2)) - 1 ) * 0.25
+    # print(i)
+    a = [1, 2, 3]
+    print(np.atleast_2d(a))
