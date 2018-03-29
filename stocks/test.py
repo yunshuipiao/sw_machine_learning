fc8b1f8f802045e93c887cec552ac0e86c700133
swensun <1416316137@qq.com>
2018-03-29 21:00:29 +0800

    update stocks

--- stocks/test.py
+++ stocks/test.py
@@ -19,8 +19,11 @@ import tushare as ts
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-length = 100
+sns.set()
+
+length = 30
 
 from sklearn import ensemble, linear_model, tree, svm, neighbors
 from sklearn.model_selection import train_test_split
@@ -32,37 +35,38 @@ class Stock:
         self.name = name
 
 stocks_code = [
-    Stock("keda", "002230"),
-    Stock("hengsheng", "600570"),
-    Stock("ziguangguoxin", "002049"),
-    Stock("longjigufen", "601012"),
-    # Stock("360     ", "601360"),
-    Stock("shangpinzhaipei", "300616"),
-    # Stock("腾讯控股", "H00700"),
-    Stock("dongfangwangli", "300367"),
-    Stock("yiligufen", "600887"),
-    Stock("yongyouwangluo", "600588"),
-    Stock("dongfangguoxin", "300166"),
-    Stock("zhaoshangyinhang", "600036"),
-    Stock("zhongguopinan", "601318"),
-    Stock("shengheziyuan", "600392"),
+    # Stock("kedaxunfei", "002230"),
+    # Stock("hengshengdianzi", "600570"),
+    # Stock("ziguangguoxin", "002049"),
+    # Stock("longjigufen", "601012"),
+    # # Stock("360     ", "601360"),
+    # Stock("shangpinzhaipei", "300616"),
+    # # Stock("腾讯控股", "H00700"),
+    # Stock("dongfangwangli", "300367"),
+    # Stock("yiligufen", "600887"),
+    # Stock("yongyouwangluo", "600588"),
+    # Stock("dongfangguoxin", "300166"),
+    # Stock("zhaoshangyinhang", "600036"),
+    # Stock("zhongguopinan", "601318"),
+    # Stock("shengheziyuan", "600392"),
 ]
 
 if __name__ == '__main__':
     for s in stocks_code:
         stock_data = ts.get_hist_data(s.code)
-        # print(stock_data)
+        # print(stock_data.head(5))
         # print(stock_data.head(10))
         stock_data = stock_data.head(length).as_matrix()
         p_change = stock_data[:, 6]
         p_change[:-1] = p_change[1:]
         high_change = (stock_data[:, 1] - stock_data[:, 0]) / stock_data[:, 0] * 100
+        # print(high_change[0:5])
         low_change = (stock_data[:, 3] - stock_data[:, 0]) / stock_data[:, 0] * 100
+        # print(low_change[0:5])
 
-        x = np.arange(0, 100)
-        plt.figure()
-        plt.plot(x, high_change, 'r-', label="high")
-        plt.plot(x, low_change, 'g-', label="low")
+        x = np.arange(0, length)
+        plt.plot(x, high_change, "ro-", label="high")
+        plt.plot(x, low_change, "go-", label="low")
         plt.title(s.name)
         plt.legend()
         plt.show()
